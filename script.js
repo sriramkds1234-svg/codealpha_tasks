0553a92a7350d5b7753b09d52a218cc9dcc3b98e
sriramkds1234-svg <sriramkds1234@gmail.com>
2026-06-05 01:55:56 +0530

    Added Task 2 - Modern Calculator

--- script.js
+++ script.js
@@ -1,59 +1,23 @@
-const images = document.querySelectorAll(".gallery-item");
-
-let currentIndex = 0;
-
-function openLightbox(index){
-    currentIndex = index;
-
-    document.getElementById("lightbox").style.display = "flex";
-
-    document.getElementById("lightbox-img").src =
-    images[currentIndex].src;
+function appendValue(value){
+    document.getElementById("display").value += value;
 }
 
-function closeLightbox(){
-    document.getElementById("lightbox").style.display = "none";
+function clearDisplay(){
+    document.getElementById("display").value = "";
 }
 
-function nextImage(){
+function deleteLast(){
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0,-1);
+}
 
-    currentIndex++;
+function calculate(){
+    let display = document.getElementById("display");
 
-    if(currentIndex >= images.length){
-        currentIndex = 0;
+    try{
+        display.value = eval(display.value);
     }
-
-    document.getElementById("lightbox-img").src =
-    images[currentIndex].src;
-}
-
-function prevImage(){
-
-    currentIndex--;
-
-    if(currentIndex < 0){
-        currentIndex = images.length - 1;
+    catch{
+        display.value = "Error";
     }
-
-    document.getElementById("lightbox-img").src =
-    images[currentIndex].src;
-}
-
-function filterImages(category) {
-
-    const images = document.querySelectorAll('.gallery-item');
-
-    images.forEach(function(image) {
-
-        if (category === 'all') {
-            image.style.display = 'block';
-        }
-        else if (image.classList.contains(category)) {
-            image.style.display = 'block';
-        }
-        else {
-            image.style.display = 'none';
-        }
-
-    });
 }
